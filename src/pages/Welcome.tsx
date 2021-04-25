15f589089992733e72b098fb5e9795b39bff1433
unknown <jriberto.filho@gmail.com>
2021-04-25 15:11:23 -0300

    ajustes na tela e componentes

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { SafeAreaView, Text, Image, StyleSheet, Platform } from 'react-native';
+
+import wateringImg from '../assets/watering.png'
+import { Button } from '../components/Button';
+import colors from '../styles/colors';
+
+export function Welcome() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'}
+                suas plantas de {'\n'}
+                forma fácil
+            </Text>
+
+            <Image source={wateringImg} style={styles.image} />
+
+            <Text style={styles.subtitle} >
+                Não esqueça mais de regar suas plantas.
+            </Text>
+
+            <Button title="Avançar" />
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingTop: Platform.OS === 'android' ? 35 : 0
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading
+    },
+    image: {
+        width: 292,
+        height: 184
+    }
+})
