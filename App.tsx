107df5735f00fb5aca559f25da2368d8938e90d6
unknown <jriberto.filho@gmail.com>
2021-04-25 23:37:56 -0300

    tela para o usuario se identificar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
+
+import { UserIdentification } from './src/pages/UserIdentification';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +19,6 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <Welcome />
+    <UserIdentification />
   );
 }
